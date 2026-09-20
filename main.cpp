bb44dbe7ea1ff74d09f9374a998ad2ae8bd5cb1c
Arsen <dulaev.arsen2006@gmail.com>
2026-09-20 12:38:24 +0300

    done 06

--- main.cpp
+++ main.cpp
@@ -1,18 +1,12 @@
 #include <iostream>
-#include <string>
-#include <utility>
 
-std::string concat(std::string a, std::string b) {
-	// TODO: build the joined string here.
-	// a and b are your own copies -- move out of them instead of copying.
-	std::string result = std::move(a) + std::move(b);
-	return result;
+template <typename T>
+T sumPair(T a, T b) {
+	return a + b;
 }
 
 int main() {
-	std::string a, b;
-	std::getline(std::cin, a);
-	std::getline(std::cin, b);
-	std::cout << concat(std::move(a), std::move(b)) << "\n";
-	return 0;
+	int a, b;
+	std::cin >> a >> b;
+	std::cout << sumPair<int>(a, b) << std::endl;
 }
