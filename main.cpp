6cce0959d9def5d75a590d39c27cb378cd852d8a
Arsen <dulaev.arsen2006@gmail.com>
2026-09-20 12:58:36 +0300

    done 07

--- main.cpp
+++ main.cpp
@@ -1,12 +1,26 @@
 #include <iostream>
-
-template <typename T>
-T sumPair(T a, T b) {
-	return a + b;
-}
+#include <vector>
+#include <algorithm>
+#include <numeric>
+#include <sstream>
 
 int main() {
-	int a, b;
-	std::cin >> a >> b;
-	std::cout << sumPair<int>(a, b) << std::endl;
+	std::string line;
+	std::getline(std::cin, line);
+	std::istringstream iss(line);
+	std::vector<int> nums;
+	int x;
+	while (iss >> x) {
+		nums.push_back(x);
+	}
+	std::cout << "count: " << std::count_if(nums.begin(), nums.end(), [](int n) {
+		return n % 2 == 0;
+	}) << "\n";
+	nums.erase(std::remove_if(nums.begin(), nums.end(), [](int n) { return n % 2 == 1; }), nums.end());
+	std::cout << "sum: " << std::accumulate(nums.begin(), nums.end(), 0);
+	// count and sum of evens
+	// print count: <n>
+	// print sum: <s>
+
+	return 0;
 }
